80e5e1a6e0bbc53a9c33c2cd9b1a1ad2f25a5627
myaeba <lim104321@naver.com>
2025-05-26 18:18:18 +0900

    fix: Login Component 작성 방식 통일

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,11 @@
 import LoginForm from '../components/LoginForm'
 
-const LoginPage = () => (
-  <div className="flex min-h-screen flex-col items-center bg-[#f5f8fa]">
-    <div className="mt-4 ml-4 self-start text-base text-[#bdbdbd]">
-      로그인 페이지
+function LoginPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center bg-[#f5f8fa]">
+      <LoginForm />
     </div>
-    <LoginForm />
-  </div>
-)
+  )
+}
 
 export default LoginPage
